1a66b8bf29854030c551bcaabe233e2ccc0abe67
jeremyrico15 <36123157+jeremyrico15@users.noreply.github.com>
2018-12-05 10:43:18 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float loanTot, newTot, loanLength, annualInterest, dailyInterest;
+    cout << "Please input the following information:" << endl;
+    cout << "Total loan amount: ";
+    cin  >> loanTot;
+
+    cout << "Estimated length of loan (days): ";
+    cin  >> loanLength;
+
+    cout << "Annual interest rate (percentage): ";
+    cin  >> annualInterest;
+    cout << endl;
+
+    dailyInterest = annualInterest  / 36500;
+    newTot = loanTot;
+
+    for (int i = 0; i <= loanLength; i++){
+        newTot = newTot + (newTot * dailyInterest);
+    }
+
+    float interest = newTot - loanTot;
+
+    cout << "Final amount owed: $" << newTot;
+    cout << endl;
+
+    cout << "Total amount of interest: $" << interest;
+    cout << endl;
+
+    return 0;
+}
